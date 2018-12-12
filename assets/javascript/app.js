69a3ffba8e30c98f16770ada0b256964ef510d00
Matthew McGee <mrmatthewmcgee@gmail.com>
2018-12-12 00:24:17 -0500

    new

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,44 +1,35 @@
 var config = {
-    apiKey: "AIzaSyCcR-lMuoCLd4uQO0HD0fdPeXt_i76Arhw",
-    authDomain: "train-scheduler-65893.firebaseapp.com",
-    databaseURL: "https://train-scheduler-65893.firebaseio.com",
-    projectId: "train-scheduler-65893",
-    storageBucket: "train-scheduler-65893.appspot.com",
-    messagingSenderId: "879577518161"
-  };
-  
+  apiKey: "AIzaSyCcR-lMuoCLd4uQO0HD0fdPeXt_i76Arhw",
+  authDomain: "train-scheduler-65893.firebaseapp.com",
+  databaseURL: "https://train-scheduler-65893.firebaseio.com",
+  projectId: "train-scheduler-65893",
+  storageBucket: "train-scheduler-65893.appspot.com",
+  messagingSenderId: "879577518161"
+};
+
 firebase.initializeApp(config);
 
 var database = firebase.database();
 
 // 2. Button for adding Trains
 $("#add-train-btn").on("click", function(event) {
+  console.log("Hi");
   event.preventDefault();
+  event.stopPropagation();
 
   // Grabs user input
   var trainName = $("#train-name-input").val().trim();
   var destination = $("#destination-input").val().trim();
   var time = moment($("#time-input").val().trim(), "0000").format("X");
   var length = $("#frequency-input").val().trim();
-
-
-});
-
-var newTrain = {
+  var newTrain = {
     name: trainName,
     place: destination,
-    time: time, //can I call these the same thing?
-    length: length //can I call these the same thing?
+    time: time,
+    length: length
   };
 
   database.ref().push(newTrain);
-
-  // Logs everything to console
-  console.log(newTrain.name);
-  console.log(newTrain.place);
-  console.log(newTrain.time);
-  console.log(newTrain.length);
-
   alert("Train Successfully Added");
 
   // Clears all of the text-boxes
@@ -47,32 +38,30 @@ var newTrain = {
   $("#destination-input").val("");
   $("#frequency-input").val("");
 
-  database.ref().on("child_added", function(childSnapshot) {
-    console.log(childSnapshot.val());
-  
-    // Store everything into a variable.
-    var trainName = childSnapshot.val().name;
-    var destination = childSnapshot.val().place;
-    var time = childSnapshot.val().time;
-    var length = childSnapshot.val().length;
-  
-    // Train Info
-    console.log(trainName);
-    console.log(destination);
-    console.log(time);
-    console.log(length);
+  return false;
+});
 
-  
-    // Create the new row
-    var newRow = $("<tr>").append(
-      $("<td>").text(trainName),
-      $("<td>").text(destination),
-      $("<td>").text(time),
-      $("<td>").text(length),
-    );
-  
-    // Append the new row to the table
-    $("#train-table > tbody").append(newRow);
+
+
+database.ref().on("child_added", function (childSnapshot) {
+  console.log(childSnapshot.val());
+
+  // Store everything into a variable.
+  var trainName = childSnapshot.val().name;
+  var destination = childSnapshot.val().place;
+  var time = childSnapshot.val().time;
+  var length = childSnapshot.val().length;
+
+  // Create the new row
+  var newRow = $("<tr>").append(
+    $("<td>").text(trainName),
+    $("<td>").text(destination),
+    $("<td>").text(time),
+    $("<td>").text(length),
+  );
+
+  // Append the new row to the table
+  $("#train-table > tbody").append(newRow);
 });
 
 
