31ad4fdf3e76e39939880789e40a57286af125ba
Matthew McGee <mrmatthewmcgee@gmail.com>
2018-12-13 23:35:00 -0500

    new

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-
+// Configurues my Firebase key
 var config = {
   apiKey: "AIzaSyAaJuHoUiw7UmGIuwsLvYsitSyODn8ayJU",
   authDomain: "train-scheduler-8cd73.firebaseapp.com",
@@ -8,13 +8,14 @@ var config = {
   messagingSenderId: "880865646525"
 };
 
+// Initilizing Firebase
 firebase.initializeApp(config);
 
+// Declaing Firebase database
 var database = firebase.database();
 
-// 2. Button for adding Trains
+// Button for adding Trains
 $("#add-train-btn").on("click", function (event) {
-  console.log("Hi");
   event.preventDefault();
   event.stopPropagation();
 
@@ -30,6 +31,7 @@ $("#add-train-btn").on("click", function (event) {
     length: length
   };
 
+  //Pusshing the user train info into the into the table
   database.ref().push(newTrain);
   alert("Train Successfully Added");
 
@@ -42,6 +44,7 @@ $("#add-train-btn").on("click", function (event) {
   return false;
 });
 
+// Function for diplaying the info into the table
 database.ref().on("child_added", function (childSnapshot) {
   console.log(childSnapshot.val());
   // Store everything into a variable.
@@ -49,9 +52,25 @@ database.ref().on("child_added", function (childSnapshot) {
   var destination = childSnapshot.val().place;
   var time = childSnapshot.val().time;
   var length = childSnapshot.val().length;
-  
+  // variable for timing of trains
+  var timeArray = time.split(":");
+  var trainTime = moment().hours(timeArray[0]).minutes(timeArray[1]);
+  var maxTime = moment.max(moment(), trainTime);
+  var trainMinutes;
+  var trainArrival;
+  // If statment for measure of time calculated on the table
+  if (maxTime === trainTime) {
+    trainArrival = trainTime.format("hh:mm A");
+    trainMinutes = trainTime.diff(moment(), "minutes");
+  } else {
+    var differentTimes = moment().diff(trainTime, "minutes");
+    var remainder = differentTimes % length;
+    trainMinutes = length - remainder;
+    trainArrival = moment().add(trainMinutes, "m").format("hh:mm A");
+  }
+
   // Append the new row to the table
-  $("tbody").append("<tr><td>" + trainName + "</td><td>" + destination + "</td></tr>" + time + "</td></tr>" + length);
+  $("tbody").append("<tr><td>" + trainName + "</td><td>" + destination + "</td><td>" + time + "</td><td>" + length + "</td><td>" + trainArrival + "</td><td>" + trainMinutes + "</td></tr>");
 
 });
 
